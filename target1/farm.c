4107db0e6f3584b016ed08a7e85c380624f42be1
LJH <1306985284@qq.com>
2023-02-24 19:10:57 +0800

    finish attack lab

--- target1/farm.c
+++ target1/farm.c
@@ -4,44 +4,60 @@ int start_farm()
     return 1;
 }
 
-unsigned getval_142()
+unsigned getval_142()0x40199a
 {
     return 2425387259U;
+    0x9078fbb8
+    c390
 }
 
-unsigned addval_273(unsigned x)
+unsigned addval_273(unsigned x)0x4019a0
 {
     return x + 3284633928U;
+    0x8948878d
+    c3c3c7 
 }
 
-unsigned addval_219(unsigned x)
+unsigned addval_219(unsigned x)0x4019a7
 {
     return x + 2421715793U;
+     0x7351878d
+     c39058 //90 mean nop that won't do anything, only use for alignment
 }
 
-void setval_237(unsigned *p)
+void setval_237(unsigned *p)0x4019ae
 {
     *p = 3351742792U;
+    0x894807c7
+    c3c7c7
 }
 
-void setval_424(unsigned *p)
+void setval_424(unsigned *p)0x4019b5
 {
     *p = 2455290452U;
+    0xc25407c7 
+    c39258
 }
 
-void setval_470(unsigned *p)
+void setval_470(unsigned *p) 0x4019bc
 {
     *p = 3347925091U;
+    0x486307c7
+    c3c78d
 }
 
-void setval_426(unsigned *p)
+void setval_426(unsigned *p)0x4019c3
 {
     *p = 2428995912U;
+    0x894807c7
+    c390c7
 }
 
-unsigned getval_280()
+unsigned getval_280()0x4019ca
 {
     return 3281016873U;
+    0x905829b8
+    c3c3
 }
 
 /* This function marks the middle of the farm */
@@ -51,169 +67,202 @@ int mid_farm()
 }
 
 /* Add two arguments */
-long add_xy(long x, long y)
+long add_xy(long x, long y)4019d6
 {
     return x+y;
+    0x37048d48
+    c3
 }
 
-unsigned getval_481()
+unsigned getval_481()4019db
 {
     return 2428668252U;
+    0xc2895cb8      0x07c7c390
 }
 
-void setval_296(unsigned *p)
+void setval_296(unsigned *p)4019e1
 {
     *p = 2425409945U;
+     0xd19907c7      0x8dc39090
 }
 
-unsigned addval_113(unsigned x)
+unsigned addval_113(unsigned x)4019e8
 {
     return x + 3380137609U;
+    0xce89878d      0x8dc3c978
 }
 
-unsigned addval_490(unsigned x)
+unsigned addval_490(unsigned x)4019ef
 {
     return x + 3676361101U;
+    0xd18d878d      0xb8c3db20
 }
 
-unsigned getval_226()
+unsigned getval_226()4019f6
 {
     return 3225997705U;
+    0x48d189b8      0x07c7c3c0
 }
 
-void setval_384(unsigned *p)
+void setval_384(unsigned *p)4019fc
 {
-    *p = 3229929857U;
+    *p = 3229929857U;0xd18107c7      0x8dc3c084
 }
 
-unsigned addval_190(unsigned x)
+unsigned addval_190(unsigned x)401a03
 {
     return x + 3767093313U;
+    0x4841878d      0xc7c3e089
 }
 
-void setval_276(unsigned *p)
+void setval_276(unsigned *p)401a0a
 {
     *p = 3372794504U;
+    0xc28807c7      0x8dc3c908
 }
 
-unsigned addval_436(unsigned x)
+unsigned addval_436(unsigned x)401a11
 {
     return x + 2425409161U;
+    0xce89878d      0xb8c39090
 }
 
-unsigned getval_345()
+unsigned getval_345()401a18
 {
     return 3252717896U;
+    0xe08948b8      0x878dc3c1
 }
 
-unsigned addval_479(unsigned x)
+unsigned addval_479(unsigned x)401a1e
 {
     return x + 3372270217U;
+    0xc289878d      0x8dc3c900
 }
 
-unsigned addval_187(unsigned x)
+unsigned addval_187(unsigned x)401a25
 {
     return x + 3224948361U;
+    0xce89878d      0xc7c3c038
 }
 
-void setval_248(unsigned *p)
+void setval_248(unsigned *p)401a2c
 {
     *p = 3674787457U;
+    0xce8107c7      0xb8c3db08
 }
 
-unsigned getval_159()
+unsigned getval_159()401a33
 {
     return 3375944073U;
+    0x38d189b8      0x878dc3c9
 }
 
-unsigned addval_110(unsigned x)
+unsigned addval_110(unsigned x)401a39
 {
     return x + 3286272456U;
+     0x89c8878d      0x8dc3c3e0
 }
 
-unsigned addval_487(unsigned x)
+unsigned addval_487(unsigned x)401a40
 {
     return x + 3229926025U;
+    0xc289878d      0x8dc3c084
 }
 
-unsigned addval_201(unsigned x)
+unsigned addval_201(unsigned x)401a47
 {
     return x + 3353381192U;
+    0x8948878d      0xb8c3c7e0
 }
 
-unsigned getval_272()
+unsigned getval_272()401a4e
 {
     return 3523793305U;
+    0x08d199b8      0x89b8c3d2
 }
 
-unsigned getval_155()
+unsigned getval_155()401a54
 {
     return 3385115273U;
+    0xc4c289b8      0x07c7c3c9
 }
 
-void setval_299(unsigned *p)
+void setval_299(unsigned *p)5a
 {
     *p = 2447411528U;
+    0x894807c7      0x8dc391e0
 }
 
-unsigned addval_404(unsigned x)
+unsigned addval_404(unsigned x)61
 {
     return x + 3281178249U;
+    0xce89878d      0xb8c3c392
 }
 
-unsigned getval_311()
+unsigned getval_311()68
 {
     return 3674788233U;
+    0x08d189b8      0x07c7c3db
 }
 
-void setval_167(unsigned *p)
+void setval_167(unsigned *p)6e
 {
     *p = 3281113481U;
+     0xd18907c7      0xc7c3c391
 }
 
-void setval_328(unsigned *p)
+void setval_328(unsigned *p)75
 {
     *p = 3526935169U;
+    0xc28107c7      0xc7c3d238
 }
 
-void setval_450(unsigned *p)
+void setval_450(unsigned *p)7c
 {
     *p = 3372797449U;
+    0xce0907c7      0x8dc3c908
 }
 
-unsigned addval_358(unsigned x)
+unsigned addval_358(unsigned x)83
 {
     return x + 2430634248U;
+     0x8908878d      0x8dc390e0
 }
 
-unsigned addval_124(unsigned x)
+unsigned addval_124(unsigned x)8a
 {
     return x + 1019724425U;
+    0xc289878d      0xb8c33cc7
 }
 
-unsigned getval_169()
+unsigned getval_169()91
 {
     return 3223375496U;
+    0x20ce88b8      0x07c7c3c0
 }
 
-void setval_181(unsigned *p)
+void setval_181(unsigned *p)97
 {
     *p = 3269495112U;
+     0x894807c7      0x8dc3c2e0
 }
 
-unsigned addval_184(unsigned x)
+unsigned addval_184(unsigned x)9e
 {
     return x + 3529556617U;
+    0xc289878d      0xb8c3d260
 }
 
-unsigned getval_472()
+unsigned getval_472()a5
 {
     return 3525365389U;
+    0x20ce8db8      0x07c7c3d2
 }
 
-void setval_350(unsigned *p)
+void setval_350(unsigned *p)ab
 {
     *p = 2430634312U;
+      0x894807c7      0xb8c390e0
 }
 
 /* This function marks the end of the farm */
