a2c6a4e0c9965b463ee1a751c1fa7b0b778a4e95
LJH <1306985284@qq.com>
2023-03-02 01:21:10 +0800

    finish cachelab beside a few mistake

--- cachelab-handout/csim.c
+++ cachelab-handout/csim.c
@@ -1,7 +1,108 @@
 #include "cachelab.h"
-
-int main()
+#include <unistd.h>
+#include <math.h>
+#include <getopt.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+typedef struct
 {
-    printSummary(0, 0, 0);
+    bool valid;
+    int mark;
+    int timeStamp;
+} block;
+
+int main(int argc, char *argv[])
+{
+    int S = 0, E = 1, B = 0, opt, s, b;
+    int hit = 0, miss = 0, evictions = 0;
+    FILE *pFile;
+    while (-1 != (opt = getopt(argc, argv, "s:E:b:t:")))
+    {
+        switch (opt)
+        {
+        case 's':
+            s = atoi(optarg);
+            S = pow(2, s);
+            break;
+        case 'E':
+            E = atoi(optarg);
+            break;
+        case 'b':
+            b = atoi(optarg);
+            B = pow(2, b);
+            break;
+        case 't':
+            pFile = fopen(optarg, "r");
+            break;
+        case 'v':
+            break;
+        default:
+            printf("wrong argument\n");
+            break;
+        }
+    }
+
+    int blockSize = sizeof(block);
+    block *cache = malloc(S * E * blockSize);
+    memset(cache, 0, S * E * blockSize);
+    int maskS = (S - 1) << b;
+    int maskB = B - 1;
+
+    char identifier;
+    int address, size;
+    int timeStamp = 0;
+    while (fscanf(pFile, " %c %x,%d", &identifier, &address, &size) > 0)
+    {
+        if (identifier == 'I')
+            continue;
+        int offset = address & maskB;
+        int groupId = (address & maskS) >> b;
+        int mark = address >> (b + s);
+        printf("%x %d",groupId,offset);
+        block *p = cache + groupId * E;
+        int mintime = 1e9;
+        int eviction = 0;
+        for (int i = 0; i < E; i++)
+        {
+            if (!p->valid)
+            {
+                p->valid = true;
+                p->mark = mark;
+                p->timeStamp = timeStamp;
+                miss++;
+                if (identifier == 'M')
+                    hit++;
+                break;
+            }
+            if (p->mark == mark)
+            {
+                hit++;
+                p->timeStamp=timeStamp;
+                if (identifier == 'M')
+                    hit++;
+                break;
+            }
+            eviction = mintime < p->timeStamp ? eviction : i;
+            mintime = mintime < p->timeStamp ? mintime : p->timeStamp;
+            p++;
+            if (i == E - 1)
+            {
+                block *evict = cache + groupId * E + eviction;
+                evict->valid = true;
+                evict->mark = mark;
+                evict->timeStamp = timeStamp;
+                evictions++;
+                miss++;
+                if (identifier == 'M')
+                    hit++;
+            }
+        }
+        timeStamp++;
+    }
+    printSummary(hit, miss, evictions);
+    free(cache);
+    fclose(pFile);
     return 0;
 }
--- cachelab-handout/trans.c
+++ cachelab-handout/trans.c
@@ -1,4 +1,4 @@
-/* 
+/*
  * trans.c - Matrix transpose B = A^T
  *
  * Each transpose function must have a prototype of the form:
@@ -6,44 +6,83 @@
  *
  * A transpose function is evaluated by counting the number of misses
  * on a 1KB direct mapped cache with a block size of 32 bytes.
- */ 
+ */
 #include <stdio.h>
 #include "cachelab.h"
 
 int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 
-/* 
+/*
  * transpose_submit - This is the solution transpose function that you
  *     will be graded on for Part B of the assignment. Do not change
  *     the description string "Transpose submission", as the driver
  *     searches for that string to identify the transpose function to
- *     be graded. 
+ *     be graded.
  */
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 {
+    int temp[8][8];
+    for (int i = 0; i < M; i += 8)
+    {
+        for (int j = 0; j < N; j += 8)
+        {
+            for (int x = 0; x < 8; x++)
+            {
+                for (int y = 0; y < 8; y++)
+                {
+                    temp[x][y] = A[j + x][i + y];
+                }
+            }
+            for (int x = 0; x < 8; x++)
+            {
+                for (int y = x + 1; y < 8; y++)
+                {
+                    int t = temp[x][y];
+                    temp[x][y] = temp[y][x];
+                    temp[y][x] = t;
+                }
+            }
+            for (int x = 0; x < 8; x++)
+            {
+                for (int y = 0; y < 8; y++)
+                {
+                    B[i + x][j + y] = temp[x][y];
+                }
+            }
+        }
+    }
+    //I'm confused that I couldn't handle the situation that M/N not equal
+    //to 8 times sometime, but it work well and gain full mark in the remain
+    //two circumstances.
+    
+    if(M%8){
+        for(int i=(M/8)*8;i<M;i++){
+            for(int j=0;j<N;j++)
+            B[i][j]=A[j][i];
+        }
+    }
+    if(N%8){
+        for(int i=(N/8)*8;i<N;i++){
+            for(int j=0;j<M;j++)
+            B[j][i]=A[i][j];
+        }
+    }
+    is_transpose( M, N, A, B);
 }
 
-/* 
+/*
  * You can define additional transpose functions below. We've defined
- * a simple one below to help you get started. 
- */ 
+ * a simple one below to help you get started.
+ */
 
-/* 
+/*
  * trans - A simple baseline transpose function, not optimized for the cache.
  */
 char trans_desc[] = "Simple row-wise scan transpose";
-void trans(int M, int N, int A[N][M], int B[M][N])
+void trans(const int M,const int N, int A[N][M], int B[M][N])
 {
-    int i, j, tmp;
-
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < M; j++) {
-            tmp = A[i][j];
-            B[j][i] = tmp;
-        }
-    }    
-
+    
 }
 
 /*
@@ -56,14 +95,12 @@ void trans(int M, int N, int A[N][M], int B[M][N])
 void registerFunctions()
 {
     /* Register your solution function */
-    registerTransFunction(transpose_submit, transpose_submit_desc); 
+    registerTransFunction(transpose_submit, transpose_submit_desc);
 
     /* Register any additional transpose functions */
-    registerTransFunction(trans, trans_desc); 
-
 }
 
-/* 
+/*
  * is_transpose - This helper function checks if B is the transpose of
  *     A. You can check the correctness of your transpose by calling
  *     it before returning from the transpose function.
@@ -72,13 +109,16 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N])
 {
     int i, j;
 
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < M; ++j) {
-            if (A[i][j] != B[j][i]) {
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < M; ++j)
+        {
+            if (A[i][j] != B[j][i])
+            {
+                printf("%d %d",i,j);
                 return 0;
             }
         }
     }
     return 1;
 }
-
