35bfe1e1b1e3327bd9fb2f942fca8f07e34b10f3
LJH <1306985284@qq.com>
2023-03-10 19:16:53 +0800

    final comm

---
+++ README.md
@@ -0,0 +1,10 @@
+# self learning repo of csapp 3e  
+
+## include my solution of all labs, sources folder includes all thr raw material
+## my working environment  
+* i5 12400  
+
+* windows 11  
+
+* wsl2 unbuntu 20.04  
+
--- malloclab-handout/mm_segerate_link_list.c
+++ malloclab-handout/mm_segerate_link_list.c
@@ -14,7 +14,6 @@
 #include <assert.h>
 #include <unistd.h>
 #include <string.h>
-
 #include "mm.h"
 #include "memlib.h"
 
@@ -42,29 +41,34 @@ team_t team = {
 
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
+// use implicit link list, malloc return  immediately if the free block is less
+// than twice of the required size, cut the block if between 2-4, continue search
+// if bigger.
+/*
+ * mm_init - initialize the malloc package.
+ */
 #define WORD 4
 #define DWORD 8
 #define GET(p) (*(unsigned int *)(p))
 #define GET_POINTER(p) ((int *)(long)(GET(p)))
 #define PUT(p, val) (*(unsigned int *)(p) = (val))
 #define PUT_POINTER(p, val) (*(int **)(p) = (val))
+#define SIZE2ALIGN(size) (ALIGN(size + DWORD))
 #define GET_SIZE(p) (GET(p) >> 1)
 #define VALID(p) (GET(p) & 1)
-#define SIZE2ALIGN(size) (ALIGN(size + DWORD))
+#define PREV_P(p) (((char *)p) - GET_SIZE((char *)p - WORD))
 #define NEXT_P(p) (((char *)p) + GET_SIZE(p))
 #define NEXT_BLOCK(p) (*(int **)(((char *)p) + WORD))
 #define PREV_BLOCK(p) (*(int **)(((char *)p) + DWORD))
 #define FOOT(p) (((char *)p) + GET_SIZE(p) - WORD)
-
-int mm_init(void)
+int round2log2(int num)
 {
-    int *p = mem_sbrk(5 * WORD);
-    PUT(p, 1);
-    PUT_POINTER(p + 1, NULL);
-    PUT_POINTER(p + 2, NULL);
-    PUT(p + 3, 1);
-    PUT(p + 4, 1);
-    return 0;
+    for (int i = 3; i < 14; i++)
+    {
+        if (num <= (1 << i))
+            return i - 3;
+    }
+    return 11;
 }
 
 void bind(int *p, int *prev, int *next)
@@ -77,16 +81,32 @@ void bind(int *p, int *prev, int *next)
         PUT_POINTER(next + 2, p);
     }
 }
+
+int mm_init(void)
+{
+    int *p = mem_sbrk(12 * 4 * WORD + 4);
+    for (int i = 0; i < 12; i++)
+    {
+        PUT(p, 1);
+        PUT_POINTER(p + 1, NULL);
+        PUT_POINTER(p + 2, NULL);
+        PUT(p + 3, 1);
+        p = p + 4;
+    }
+    PUT(p, 1);
+    return 0;
+}
+
 void *mm_malloc(size_t size)
 {
     if (size == 0)
     {
         return NULL;
     }
-    int *p = (int *)mem_heap_lo();
     int align_size = SIZE2ALIGN(size);
+    int *p = (int *)mem_heap_lo() + round2log2(align_size) * 4;
     int p_size;
-     while (p != NULL && (p_size = GET_SIZE(p)) < align_size)
+    while (p != NULL && (p_size = GET_SIZE(p)) < align_size)
     {
         p = NEXT_BLOCK(p);
     }
@@ -96,23 +116,6 @@ void *mm_malloc(size_t size)
         int val = (align_size << 1) | 1;
         PUT(p, val);
         PUT(FOOT(p), val);
-        return p + 1;
-    }
-    if (p_size > 2 * align_size)
-    {
-        int remain = p_size - align_size;
-        int val = align_size << 1 | 1;
-        PUT(p, val);
-        PUT(FOOT(p), val);
-        int *next_p = (int *)NEXT_P(p);
-        val = remain << 1;
-        PUT(next_p, val);
-        PUT(FOOT(next_p), val);
-        int *next = NEXT_BLOCK(p);
-        int *prev = PREV_BLOCK(p);
-        PUT_POINTER(next_p + 1, p + 1);
-        PUT_POINTER(next_p + 2, p + 2);
-        bind((int *)next_p, prev, next);
         return p + 1;
     }
     PUT(p, GET(p) | 0x1);
@@ -125,29 +128,45 @@ void *mm_malloc(size_t size)
     return p + 1;
 }
 
-void mm_free(void *pt)
+void mm_free(void *ptr)
 {
-    int *p = (int *)pt - 1;
+    int *p = (int *)ptr - 1;
     if (VALID(p - 1))
     {
         PUT(p, GET(p) & ~1);
         PUT(FOOT(p), GET(p));
-        int *prev = mem_heap_lo();
+        int size = GET_SIZE(p);
+        int *prev = (int *)mem_heap_lo() + 4 * round2log2(size);
         int *next = NEXT_BLOCK(prev);
+        while (next != NULL && GET_SIZE(next) < size)
+        {
+            prev = next;
+            next = NEXT_BLOCK(prev);
+        }
         bind(p, prev, next);
         return;
     }
     int prev_size = GET_SIZE(p - 1);
     int *prev_p = (int *)(((char *)p) - prev_size);
-    int size = GET_SIZE(p);
-    int val = (prev_size + size) << 1;
+    int size = prev_size + GET_SIZE(p);
+    int val = (size) << 1;
     PUT(prev_p, val);
     PUT(FOOT(p), val);
+    int *next = NEXT_BLOCK(prev_p);
+    int *prev = PREV_BLOCK(prev_p);
+    PUT_POINTER(prev + 1, next);
+    if (next != NULL)
+        PUT_POINTER(next + 2, prev);
+    prev = (int *)mem_heap_lo() + round2log2(size) * 4;
+    next = NEXT_BLOCK(prev);
+    while (next != NULL && GET_SIZE(next) < size)
+    {
+        prev = next;
+        next = NEXT_BLOCK(prev);
+    }
+    bind(prev_p, prev, next);
 }
 
-/*
- * mm_realloc - Implemented simply in terms of mm_malloc and mm_free
- */
 void *mm_realloc(void *ptr, size_t size)
 {
     if (0)
@@ -165,20 +184,30 @@ void *mm_realloc(void *ptr, size_t size)
     mm_free(ptr);
     return p;
 }
+void print_heap()
+{
+    int *p = mem_heap_lo();
+    for (int i = 0; i < 12; i++)
+    {
+        for (int *ptr = p; ptr != NULL; ptr = NEXT_BLOCK(ptr))
+            printf("%x\t%d\t%x\t%x\t%d\n", ptr, *ptr, *(ptr + 1), *(ptr + 2), *(int *)FOOT(ptr));
+        p += 4;
+    }
+}
 /*
 trace  valid  util     ops      secs  Kops
- 0       yes   85%    5694  0.000064 89388
- 1       yes   89%    5848  0.000058100481
- 2       yes   90%    6648  0.000077 86226
- 3       yes   95%    5380  0.000055 97641
- 4       yes   66%   14400  0.000088164196
- 5       yes   84%    4800  0.000120 39867
- 6       yes   83%    4800  0.000137 35011
- 7       yes   55%   12000  0.005763  2082
- 8       yes   51%   24000  0.020194  1188
- 9       yes   55%   14401  0.000097149233
-10       yes   40%   14401  0.000050289177
-Total          72%  112372  0.026702  4208
+ 0       yes   54%    5694  0.000074 76946
+ 1       yes   47%    5848  0.000084 69619
+ 2       yes   68%    6648  0.000072 92333
+ 3       yes   80%    5380  0.000059 91966
+ 4       yes    0%   14400  0.000138104197
+ 5       yes   82%    4800  0.000096 50157
+ 6       yes   80%    4800  0.000097 49587
+ 7       yes   55%   12000  0.000086139860
+ 8       yes   51%   24000  0.000158152284
+ 9       yes   29%   14401  0.000182 79257
+10       yes    4%   14401  0.000096150167
+Total          50%  112372  0.001140 98555
 
-Perf index = 43 (util) + 40 (thru) = 83/100
+Perf index = 30 (util) + 40 (thru) = 70/100
 */
